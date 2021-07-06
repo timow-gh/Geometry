1ce6063086cfb053e757451440fd665b4e2e8c18
timow-gh <39085531+timow-gh@users.noreply.github.com>
2021-07-06 22:13:27 +0200

    Added PolyLine.hpp

---
+++ src/include/Geometry/PolyLine.hpp
@@ -0,0 +1,42 @@
+#ifndef GEOMETRY_POLYLINE_H
+#define GEOMETRY_POLYLINE_H
+
+#include <LinAl/LinearAlgebra.hpp>
+
+namespace Geometry
+{
+template <typename T, std::size_t D>
+class PolyLine
+{
+    LinAl::VecVector<T, D> m_points;
+
+  public:
+    PolyLine(const LinAl::VecVector<T, D>& mPoints);
+    [[nodicard]] const LinAl::VecVector<T, D>& getPoints() const;
+};
+
+template <typename T, std::size_t D>
+PolyLine<T, D>::PolyLine(const LinAl::VecVector<T, D>& mPoints)
+    : m_points(mPoints)
+{
+}
+
+template <typename T, std::size_t D>
+const LinAl::VecVector<T, D>& PolyLine<T, D>::getPoints() const
+{
+    return m_points;
+}
+
+template <typename T>
+using PolyLine2 = PolyLine<T, 2>;
+using PolyLine2f = PolyLine2<float_t>;
+using PolyLine2d = PolyLine2<double_t>;
+
+template <typename T>
+using PolyLine3 = PolyLine<T, 3>;
+using PolyLine3f = PolyLine3<float_t>;
+using PolyLine3d = PolyLine3<double_t>;
+
+} // namespace Geometry
+
+#endif // GEOMETRY_POLYLINE_H
---
+++ tests/Test_PolyLine.cpp
@@ -0,0 +1,25 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
+
+#include "Geometry/PolyLine.hpp"
+#include "gtest/gtest.h"
+
+using namespace LinAl;
+using namespace Geometry;
+
+TEST(PolyLine, Ctor)
+{
+    Vec3d first = {0, 0, 0};
+    Vec3d second = {0, 0, 0};
+
+    Vec3dVector points = {first, second};
+    PolyLine3d polyLine = PolyLine3d(points);
+    Vec3dVector polyLinePoints = polyLine.getPoints();
+
+    EXPECT_EQ(first, polyLinePoints[0]);
+    EXPECT_EQ(second, polyLinePoints[1]);
+}
+
+
+
+#pragma clang diagnostic pop
