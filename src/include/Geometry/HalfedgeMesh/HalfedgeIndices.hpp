9d193e801f462fa5ecbed48619291d12e36be838
timow-gh <39085531+timow-gh@users.noreply.github.com>
2021-10-18 22:40:40 +0200

    Draw segments for all halfedges of spheres.

---
+++ src/include/Geometry/HalfedgeMesh/HalfedgeIndices.hpp
@@ -0,0 +1,34 @@
+#ifndef FILAPP_HALFEDGEINDICES_HPP
+#define FILAPP_HALFEDGEINDICES_HPP
+
+#include <Core/Types/TVector.hpp>
+#include <Geometry/HalfedgeMesh/Halfedge.hpp>
+#include <Geometry/HalfedgeMesh/HalfedgeMesh.hpp>
+
+namespace Geometry
+{
+
+struct SegmentIndices
+{
+    uint32_t source;
+    uint32_t target;
+
+    SegmentIndices(uint32_t source, uint32_t target)
+        : source(source), target(target)
+    {
+    }
+};
+
+template <typename T>
+Core::TVector<SegmentIndices> calcSegmentIndices(const HalfedgeMesh<T>& mesh)
+{
+    Core::TVector<SegmentIndices> result;
+    for (const Halfedge<T>& halfedge: mesh.getHalfedges())
+        result.push_back(SegmentIndices(halfedge.getVertexIndex(),
+                                        halfedge.next()->getVertexIndex()));
+    return result;
+}
+
+} // namespace Geometry
+
+#endif // FILAPP_HALFEDGEINDICES_HPP
