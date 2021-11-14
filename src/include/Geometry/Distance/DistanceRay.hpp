31c4dc923ed764da0730f976f955e604dab18224
timow-gh <39085531+timow-gh@users.noreply.github.com>
2021-11-14 18:41:26 +0100

    Removed ray-line distance calculation. WIP

--- src/include/Geometry/Distance/DistanceRay.hpp
+++ src/include/Geometry/Distance/DistanceRay.hpp
@@ -31,39 +31,6 @@ template <typename T, std::size_t D>
     return distance(ray, vec);
 }
 
-template <typename T, std::size_t D>
-[[nodiscard]] constexpr T distance(const Ray<T, D>& ray, const Line<T, D>& line)
-{
-    // TODO parallel
-    const auto& rayOrigin = ray.getOrigin();
-    const auto& lineOrigin = line.getOrigin();
-    LinAl::Vec<T, D> originVec{rayOrigin - lineOrigin};
-
-    const auto& lineDir = line.getDirection();
-    const auto& rayDir = ray.getDirection();
-
-    T rayOriginVec = LinAl::dot(rayDir, originVec);
-    T rayLine = -LinAl::dot(rayDir, lineDir);
-//    T rayRay = LinAl::dot(rayDir, rayDir);
-//    T rayParam = (-rayOriginVec + rayLine) / rayRay;
-    T rayParam = (rayOriginVec + rayLine);
-    rayParam = std::max(0.0, rayParam);
-    LinAl::Vec<T, D> rayPoint = rayOrigin + rayParam * rayDir;
-
-    T lineOriginVec = LinAl::dot(lineDir, originVec);
-//    T lineLine = LinAl::dot(lineDir, lineDir);
-//    T lineParam = lineOriginVec / lineLine;
-    LinAl::Vec<T, D> linePoint = lineOrigin + lineOriginVec * lineDir;
-
-    return LinAl::norm2(LinAl::Vec<T, D>{rayPoint - linePoint});
-}
-
-template <typename T, std::size_t D>
-[[nodiscard]] constexpr T distance(const Line<T, D>& line, const Ray<T, D>& ray)
-{
-    return distance(ray, line);
-}
-
 } // namespace Geometry
 
 #endif // GEOMETRY_DISTANCERAY_HPP
