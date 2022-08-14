ea3846513dc5c2d2ea99d7e3d859c27ab21a109b
timow-gh <39085531+timow-gh@users.noreply.github.com>
2022-08-14 21:01:07 +0200

    AABB

--- tests/Intersection/Test_Intersection_AABB.cpp
+++ tests/Intersection/Test_Intersection_AABB.cpp
@@ -25,3 +25,10 @@ TEST(AABBTest2f_Intersection, notIntersecting)
     AABB2f aabb2{{2.0f, 0}, 0.5f};
     EXPECT_FALSE(Geometry::isIntersecting(aabb1, aabb2));
 }
+
+TEST(AABBTest2f_Intersection, corner)
+{
+    AABB2f aabb1{{0, 0}, 0.5f};
+    AABB2f aabb2{{1.0f, 1.0f}, 0.5f};
+    EXPECT_TRUE(Geometry::isIntersecting(aabb1, aabb2));
+}
