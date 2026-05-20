5ecede235156f335a4608ade527ba83a33597030
timow-gh <39085531+timow-gh@users.noreply.github.com>
2026-05-20 22:33:17 +0200

    Fix variable length

--- test/Intersection/IntersectionIntervalTest.cpp
+++ test/Intersection/IntersectionIntervalTest.cpp
@@ -6,48 +6,48 @@ using namespace Geometry;
 
 TEST(Interval, Disjunct_LHS)
 {
-    Interval a{0.0, 1.0};
-    Interval b{-3.0, -2.0};
+    Interval lhs{0.0, 1.0};
+    Interval rhs{-3.0, -2.0};
     Interval<double> result{};
-    uint32_t res = Geometry::intersect(a, b, result);
+    uint32_t res = Geometry::intersect(lhs, rhs, result);
     EXPECT_EQ(res, 0);
 }
 
 TEST(Interval, Disjunct_RHS)
 {
-    Interval a{0.0, 1.0};
-    Interval b{2.0, 3.0};
+    Interval lhs{0.0, 1.0};
+    Interval rhs{2.0, 3.0};
     Interval<double> result{};
-    uint32_t res = Geometry::intersect(a, b, result);
+    uint32_t res = Geometry::intersect(lhs, rhs, result);
     EXPECT_EQ(res, 0);
 }
 
 TEST(Interval, Touching_LHS)
 {
-    Interval a{0.0, 1.0};
-    Interval b{-1.0, 0.0};
+    Interval lhs{0.0, 1.0};
+    Interval rhs{-1.0, 0.0};
     Interval<double> result{};
-    uint32_t res = Geometry::intersect(a, b, result);
+    uint32_t res = Geometry::intersect(lhs, rhs, result);
     EXPECT_EQ(res, 1);
     EXPECT_EQ(result.get_start(), 0.0);
 }
 
 TEST(Interval, Touching_RHS)
 {
-    Interval a{0.0, 1.0};
-    Interval b{1.0, 2.0};
+    Interval lhs{0.0, 1.0};
+    Interval rhs{1.0, 2.0};
     Interval<double> result{};
-    uint32_t res = Geometry::intersect(a, b, result);
+    uint32_t res = Geometry::intersect(lhs, rhs, result);
     EXPECT_EQ(res, 1);
     EXPECT_EQ(result.get_start(), 1.0);
 }
 
 TEST(Interval, Intersecting)
 {
-    Interval a{0.0, 1.0};
-    Interval b{0.5, 2.0};
+    Interval lhs{0.0, 1.0};
+    Interval rhs{0.5, 2.0};
     Interval<double> result{};
-    uint32_t res = Geometry::intersect(a, b, result);
+    uint32_t res = Geometry::intersect(lhs, rhs, result);
     EXPECT_EQ(res, 2);
     EXPECT_EQ(result, (Interval{0.5, 1.0}));
 }
