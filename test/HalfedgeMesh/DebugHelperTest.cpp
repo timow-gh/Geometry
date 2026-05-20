a51a558cc43e36432587a4096c3794a12447df16
timow-gh <39085531+timow-gh@users.noreply.github.com>
2026-05-20 21:50:17 +0200

    Fix suffixes

--- test/HalfedgeMesh/DebugHelperTest.cpp
+++ test/HalfedgeMesh/DebugHelperTest.cpp
@@ -16,20 +16,20 @@ protected:
 TEST_F(DebugHelperTest, print_inline_linal_vec)
 {
   std::ostringstream oss;
-  print_inline(oss, linal::vec<float, 3>{1.0f, 2.0f, 3.0f});
+  print_inline(oss, linal::vec<float, 3>{1.0F, 2.0F, 3.0F});
   EXPECT_EQ(oss.str(), "vec: 1 2 3");
 }
 
 TEST_F(DebugHelperTest, print_linal_vec)
 {
   std::ostringstream oss;
-  print(oss, linal::vec<float, 3>{1.0f, 2.0f, 3.0f});
+  print(oss, linal::vec<float, 3>{1.0F, 2.0F, 3.0F});
   EXPECT_EQ(oss.str(), "vec: 1 2 3\n");
 }
 
 TEST_F(DebugHelperTest, print_linal_vecs)
 {
-  std::vector<linal::vec<float, 3>> vecs = {linal::vec<float, 3>{1.0f, 2.0f, 3.0f}, linal::vec<float, 3>{4.0f, 5.0f, 6.0f}};
+  std::vector<linal::vec<float, 3>> vecs = {linal::vec<float, 3>{1.0F, 2.0F, 3.0F}, linal::vec<float, 3>{4.0F, 5.0F, 6.0F}};
   std::ostringstream oss;
   print(oss, vecs);
   EXPECT_EQ(oss.str(), "vec: 1 2 3\nvec: 4 5 6\n");
@@ -38,7 +38,7 @@ TEST_F(DebugHelperTest, print_linal_vecs)
 TEST_F(DebugHelperTest, print_vertex)
 {
   MeshBuilder<float, std::size_t> builder;
-  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0f, 2.0f, 3.0f}, linal::float3{4.0f, 5.0f, 6.0f}});
+  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0F, 2.0F, 3.0F}, linal::float3{4.0F, 5.0F, 6.0F}});
   std::ostringstream oss;
   print(oss, mesh->get_vertex(0));
   EXPECT_EQ(oss.str(), "Vertex index: 0; vec: 1 2 3\n");
@@ -47,7 +47,7 @@ TEST_F(DebugHelperTest, print_vertex)
 TEST_F(DebugHelperTest, print_vertices)
 {
   MeshBuilder<float, std::size_t> builder;
-  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0f, 2.0f, 3.0f}, linal::float3{4.0f, 5.0f, 6.0f}});
+  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0F, 2.0F, 3.0F}, linal::float3{4.0F, 5.0F, 6.0F}});
 
   using Vertex_t = Vertex<MeshTraits<float, std::size_t>>;
 
@@ -60,7 +60,7 @@ TEST_F(DebugHelperTest, print_vertices)
 TEST_F(DebugHelperTest, print_inline_facet)
 {
   MeshBuilder<float, std::size_t> builder;
-  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0f, 2.0f, 3.0f}, linal::float3{4.0f, 5.0f, 6.0f}});
+  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0F, 2.0F, 3.0F}, linal::float3{4.0F, 5.0F, 6.0F}});
   std::ostringstream oss;
   print_inline(oss, mesh->get_facet(0));
   EXPECT_EQ(oss.str(), "Facet halfedge index: 0");
@@ -69,7 +69,7 @@ TEST_F(DebugHelperTest, print_inline_facet)
 TEST_F(DebugHelperTest, print_facet)
 {
   MeshBuilder<float, std::size_t> builder;
-  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0f, 2.0f, 3.0f}, linal::float3{4.0f, 5.0f, 6.0f}});
+  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0F, 2.0F, 3.0F}, linal::float3{4.0F, 5.0F, 6.0F}});
   std::ostringstream oss;
   print(oss, mesh->get_facet(0));
   EXPECT_EQ(oss.str(), "Facet halfedge index: 0\n");
@@ -78,7 +78,7 @@ TEST_F(DebugHelperTest, print_facet)
 TEST_F(DebugHelperTest, print_facets)
 {
   MeshBuilder<float, std::size_t> builder;
-  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0f, 2.0f, 3.0f}, linal::float3{4.0f, 5.0f, 6.0f}});
+  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0F, 2.0F, 3.0F}, linal::float3{4.0F, 5.0F, 6.0F}});
 
   using Facet_t = Facet<MeshTraits<float, std::size_t>>;
 
@@ -91,7 +91,7 @@ TEST_F(DebugHelperTest, print_facets)
 TEST_F(DebugHelperTest, print_halfedge)
 {
   MeshBuilder<float, std::size_t> builder;
-  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0f, 2.0f, 3.0f}, linal::float3{4.0f, 5.0f, 6.0f}});
+  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0F, 2.0F, 3.0F}, linal::float3{4.0F, 5.0F, 6.0F}});
   std::ostringstream oss;
   print(oss, mesh->get_halfedge(0));
   EXPECT_EQ(oss.str(),
@@ -101,7 +101,7 @@ TEST_F(DebugHelperTest, print_halfedge)
 TEST_F(DebugHelperTest, print_halfedges)
 {
   MeshBuilder<float, std::size_t> builder;
-  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0f, 2.0f, 3.0f}, linal::float3{4.0f, 5.0f, 6.0f}});
+  auto mesh = builder.build(Cuboid<float>{linal::float3{1.0F, 2.0F, 3.0F}, linal::float3{4.0F, 5.0F, 6.0F}});
 
   using Halfedge_t = Halfedge<MeshTraits<float, std::size_t>>;
 
