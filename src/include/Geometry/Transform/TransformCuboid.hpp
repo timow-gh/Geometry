03db1e84cf4ea5f889c940eb6f386f31aff0b3e2
timow-gh <39085531+timow-gh@users.noreply.github.com>
2026-07-02 21:23:30 +0200

    fix: resolve clang-tidy issues by updating transform functions and adding hvec_to_vec utility

--- src/include/Geometry/Transform/TransformCuboid.hpp
+++ src/include/Geometry/Transform/TransformCuboid.hpp
@@ -2,6 +2,7 @@
 #define GEOMETRY_TRANSFORMCUBOID_HPP
 
 #include "Geometry/Cuboid.hpp"
+#include "Geometry/Transform/TransformVec.hpp"
 #include "Geometry/Utils/Compiler.hpp"
 #include <LinAl/LinearAlgebra.hpp>
 
@@ -12,7 +13,7 @@ template <typename T>
 GEO_NODISCARD constexpr Cuboid<T> transform(const Cuboid<T>& cuboid, const linal::hmat<T>& trafo) noexcept
 {
   linal::hvec<T> origin = trafo * linal::to_hvec<T, 3>(cuboid.get_origin());
-  return Cuboid<T>{linal::to_vec<T, D>(origin), cuboid.get_side_vectors()};
+  return Cuboid<T>{detail::hvec_to_vec<T, 3>(origin), cuboid.get_side_vectors()};
 }
 
 } // namespace Geometry
